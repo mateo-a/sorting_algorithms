273fce454b00ff43847977f5036670fac1619ebb
mateo-a <alexander.urrego@gmail.com>
2020-02-03 12:25:58 -0500

    first commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ *  swap - function to swap values in array
+ *  @currentp: current position
+ *  @nextp: next position
+ */
+void swap(int *currentp, int *nextp)
+{
+	int temp = *currentp;
+	*currentp = *nextp;
+	*nextp = temp;
+}
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm
+ *  @array: array to sort
+ *  @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (array == NULL || size < 1)
+		return;
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+}
